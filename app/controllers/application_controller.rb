b5e4ab9a4f36e76eca4d7e30ec4921a6cdabcd20
maksdy <diachenko.maksym@gmail.com>
2018-10-17 11:58:58 +0300

    kytsya

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-  	render plain: "hello, world!"
+  	render plain: "Кыця, я тебя люблю"
   end
 end
 
