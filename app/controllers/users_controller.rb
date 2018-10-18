d7836fb2519b42021ffdcaeb15366956a093341a
Maksym Dy <diachenko.maksym@gmail.com>
2018-10-18 22:24:00 +0300

    Working with form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  
+  def show
+  	@user = User.find(params[:id])
+  	
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])        # Не окончательная реализация!
+    if @user.save
+     # Обработать успешное сохранение.
+    else
+    render 'new'
+    end
   end
 end
