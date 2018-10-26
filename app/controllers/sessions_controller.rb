a231cbda8d275718e64700e7e45557a4b089741a
maksdy <diachenko.maksym@gmail.com>
2018-10-26 17:26:43 +0300

    Awesome

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ def create
   if user && user.authenticate(params[:session][:password])
     log_in user
     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    end
     redirect_to user
   else
     flash.now[:danger] = 'Invalid email/password combination'
@@ -18,4 +17,5 @@ end
 def destroy
   log_out if logged_in?
   redirect_to root_url
+end
 end
